0f7ddebadb18e77d50727ccb10ca3c4cf2a4f1e3
Mateusz <skipper126@interia.pl>
2020-08-20 22:38:57 +0200

    add furthest sheep from dog distance computation

--- herding/reward/furthest_sheep/furthest_sheep.cl
+++ herding/reward/furthest_sheep/furthest_sheep.cl
@@ -1,24 +1,40 @@
 
 __kernel void get_furthest_sheep_distance(__global float (*sheep_positions)[2],
                                           __global float (*target_position),
+                                          __global float (*dogs_positions)[3],
                                           __global int (*furthest_distance))
 {
     __local int distance;
+    __local int distance_to_dog;
     int id = get_global_id(0);
     if (id == 0)
+    {
         distance = 0;
+        distance_to_dog = 0;
+    }
     barrier(CLK_LOCAL_MEM_FENCE);
 
     float sheep_pos_x = sheep_positions[id][0];
     float sheep_pos_y = sheep_positions[id][1];
     float target_pos_x = target_position[0];
     float target_pos_y = target_position[1];
+    float dog_pos_x = dogs_positions[0][0];
+    float dog_pos_y = dogs_positions[0][1];
+
 
     int tmp_distance = (int)sqrt(pow(sheep_pos_x - target_pos_x, 2) +
                                pow(sheep_pos_y - target_pos_y, 2));
+
+    int tmp_distance_to_dog = (int)sqrt(pow(sheep_pos_x - dog_pos_x, 2) +
+                               pow(sheep_pos_y - dog_pos_y, 2));
+
     atomic_max(&distance, tmp_distance);
+    atomic_max(&distance_to_dog, tmp_distance_to_dog);
 
     barrier(CLK_LOCAL_MEM_FENCE);
     if(id == 0)
+    {
         *furthest_distance = distance;
+        *(furthest_distance + 1) = distance_to_dog;
+    }
 }
--- herding/reward/furthest_sheep/furthest_sheep.py
+++ herding/reward/furthest_sheep/furthest_sheep.py
@@ -12,12 +12,13 @@ class FurthestSheepRewardCounter(RewardCounter):
         self.start_distance = 0
         self.previous_distance = 0
         self.total_reward = 0
-        self.distance_buffer = env_data.ocl.create_buffer((1,), np.int32)
+        self.distance_buffer = env_data.ocl.create_buffer((2,), np.int32)
         self.reward_module = env_data.ocl.create_module(
                                                   'herding/reward/furthest_sheep/furthest_sheep.cl',
                                                   'get_furthest_sheep_distance',
                                                   [env_data.shared_buffers.sheep_positions,
                                                    env_data.shared_buffers.target_position,
+                                                   env_data.shared_buffers.dogs_positions,
                                                    self.distance_buffer])
 
     def get_reward(self):
@@ -25,7 +26,7 @@ class FurthestSheepRewardCounter(RewardCounter):
             self.total_reward = self.target_reward  # Trigger is_done flag
             return 0
 
-        furthest_sheep_distance = self._get_furthest_sheep_distance()
+        furthest_sheep_distance, distance_from_dog = self._get_furthest_sheep_distance()
 
         reward = (self.previous_distance - furthest_sheep_distance) * self.target_reward / self.start_distance
         self.previous_distance = furthest_sheep_distance
@@ -36,18 +37,17 @@ class FurthestSheepRewardCounter(RewardCounter):
         return self.total_reward >= self.target_reward - 0.5  # some space for numeric error
 
     def reset(self):
-        furthest_sheep_distance = self._get_furthest_sheep_distance()
+        furthest_sheep_distance, _ = self._get_furthest_sheep_distance()
         self.start_distance = self.previous_distance = furthest_sheep_distance
         self.total_reward = 0
 
     def _get_furthest_sheep_distance(self):
         self.reward_module.run((self.sheep_count,), (self.sheep_count,))
-        furthest_sheep_distance = self.distance_buffer.map_read()[0]
+        furthest_sheep_distance, distance_from_dog = self.distance_buffer.map_read()
         self.distance_buffer.unmap()
 
         furthest_sheep_distance = max(0, furthest_sheep_distance - self.herd_target_radius)
-
-        return furthest_sheep_distance
+        return furthest_sheep_distance, distance_from_dog
 
     def get_episode_reward(self):
         return self.total_reward
